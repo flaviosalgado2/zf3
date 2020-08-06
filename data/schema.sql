c95b178d345997db889e76212e1ef35afadb2efa
flaviosalgado2 <flaviosalgado2@hotmail.com>
2020-08-05 22:06:22 -0300

    primeiro upload

---
+++ data/schema.sql
@@ -0,0 +1,3 @@
+create table post (id INTEGER PRIMARY KEY AUTOINCREMENT), title varchar(255) not null, content text not null;
+
+insert into post(title, content) values ('post 1', 'content 1');
---
+++ module/Blog/src/Module.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Blog;
+
+use Zend\Db\TableGateway\TableGateway;
+
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . "/../config/module.config.php";
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                Model\PostTable::class => function ($container) {
+                    $tableGateway = $container->get(Model\PostTableGateway::class);
+                    return new Model\PostTable($tableGateway);
+                },
+                Model\PostTableGateway::class => function ($container) {
+                    $dbAdapter = $container->get(AdapterInterface::class);
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Model\Post());
+                    return new TableGateway('post', $dbAdapter, null, $resultSetPrototype);
+                }
+            ]
+        ];
+    }
+
+}
